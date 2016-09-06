4f8c45f4fe0a0c316e6c294a7744dc8b4a98532a
Brian Holman <zbholman@gmail.com>
2016-09-06 15:51:48 -0400

    removed unused import

--- LightingSystem/AndroidApplication/LightingSystem/app/src/main/java/com/ist440/lightingsystem/LightingOptions.java
+++ LightingSystem/AndroidApplication/LightingSystem/app/src/main/java/com/ist440/lightingsystem/LightingOptions.java
@@ -1,6 +1,5 @@
 package com.ist440.lightingsystem;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CompoundButton;
