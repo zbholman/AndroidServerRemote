7d09b431e5b35129fcdca55ded48e740902f8012
Joe Oakes <jxo19@psu.edu>
2016-09-21 17:02:28 -0400

    added JSON

---
+++ joeExamples/android/json/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.josephoakes.jsonwriter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import android.os.Message;
+import java.io.IOException;
+import android.util.JsonWriter;
+import android.widget.TextView;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView text1 = (TextView) findViewById(R.id.TextView1);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                JSONObject jsonObj = new JSONObject();
+                JSONArray jsonArray = new JSONArray();
+                try {
+                    jsonObj.put("id", 12345);
+                    jsonObj.put("name", "Climate System");
+
+                    jsonArray.put("system1");
+                    jsonArray.put("system2");
+
+                    jsonObj.put("sys", jsonArray);
+
+                    text1.setText(jsonObj.toString());
+
+                }catch (Exception ex)
+                {
+                    ex.printStackTrace();
+
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
