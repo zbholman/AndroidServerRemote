6a57be23272b54c2919cb8af59a3583f189670b3
Your Name <niravhpatel1990@gmail.com>
2016-09-26 12:42:29 -0400

    Added Android Json for temperature

---
+++ ClimateControlSystem/Android/ClimateControlJson/app/src/main/java/com/example/niravpatel/climatecontroljson/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.niravpatel.climatecontroljson;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.JsonWriter;
+import android.widget.Button;
+import android.widget.TextView;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView text1 = (TextView) findViewById(R.id.TextView1);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Button up = (Button) findViewById(R.id.upButton);
+        Button down = (Button) findViewById(R.id.downButton);
+
+        up.setOnClickListener(new View.OnClickListener() {
+            int counter;
+
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                 counter ++;
+
+                JSONObject jsonObj = new JSONObject();
+                JSONArray jsonArray = new JSONArray();
+
+                try {
+                    jsonObj.put("temp", 55 + counter);
+                    jsonObj.put("name", "Climate System");
+
+                    text1.setText(jsonObj.toString());
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+
+                }
+            }
+        });
+
+        down.setOnClickListener(new View.OnClickListener() {
+            int counter;
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                counter ++;
+
+                JSONObject jsonObj = new JSONObject();
+                JSONArray jsonArray = new JSONArray();
+
+                try {
+                    jsonObj.put("temp", 55 - counter);
+                    jsonObj.put("name", "Climate System");
+
+                    text1.setText(jsonObj.toString());
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+
+                }
+            }
+        });
+
+
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
