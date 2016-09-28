ec468fbae81a829ce35d68cabb5e0a27608090d8
cjackychen <jzc5521@psu.edu>
2016-09-28 19:22:00 -0400

    Update displayTemp.py

--- ClimateControlSystem/displayTemp.py
+++ ClimateControlSystem/displayTemp.py
@@ -1,20 +1,14 @@
-from sense_hat import SenseHat
-sense = SenseHat()
+import grovepi
+	
+	sensor = 4  
+	blue = 0    # The Blue colored sensor.
+	white = 1   # The White colored sensor.
+	
 
-while True:
-    t = sense.get_temperature()
-    p = sense.get_pressure()
-    h = sense.get_humidity()
+	while True:
+	    try:
+	        [temp,humidity] = grovepi.dht(sensor,blue)  
+	        print("temp = %.02f C humidity =%.02f%%"%(temp, humidity))
+	    except IOError:
+	        print ("Error")
 
-    t = round(t, 1)
-    p = round(p, 1)
-    h = round(h, 1)
-
-    if t > 18.3 and t < 26.7:
-        bg = [0, 100, 0]  # green
-    else:
-        bg = [100, 0, 0]  # red
-
-    msg = "Temperature = {0}, Pressure = {1}, Humidity = {2}".format(t, p, h)
-
-    sense.show_message(msg, scroll_speed=0.05, back_colour=bg)
