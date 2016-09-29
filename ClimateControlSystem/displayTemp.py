f6ada53f17c2ed28d69e1fdc43c3b197c7b4ceee
cjackychen <jzc5521@psu.edu>
2016-09-29 18:43:54 -0400

    Update displayTemp.py

--- ClimateControlSystem/displayTemp.py
+++ ClimateControlSystem/displayTemp.py
@@ -1,14 +1,10 @@
+
 import grovepi
-	
-	sensor = 4  
-	blue = 0    # The Blue colored sensor.
-	white = 1   # The White colored sensor.
-	
+sensor = 4
 
-	while True:
-	    try:
-	        [temp,humidity] = grovepi.dht(sensor,blue)  
-	        print("temp = %.02f C humidity =%.02f%%"%(temp, humidity))
-	    except IOError:
-	        print ("Error")
+try:
+        [temp,humidity] = grovepi.dht(sensor,1)
+        print temp 
 
+except IOError:
+        print "Error"
