6be1c1312b4829ba580e9858cbe5ad51373619e9
qilijian <qqj5004@psu.edu>
2016-09-30 18:10:36 -0400

    Fixed

--- BrakingSystem/Android/app/src/main/java/com/ist440/android/MainActivity.java
+++ BrakingSystem/Android/app/src/main/java/com/ist440/android/MainActivity.java
@@ -68,12 +68,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 try {
                     jsonObj.put("id", "car440brs");
                     jsonObj.put("name", "Braking System");
-                    jsonObj.put("OID", "brs");
-                    jsonObj.put("DID", "lis");
-                    jsonObj.put("CKS", "  ");
+
 
                     jsonArray.put("Braking System");
-                    jsonArray.put("Lighting System");
+
 
                     jsonObj.put("sys", jsonArray);
 
