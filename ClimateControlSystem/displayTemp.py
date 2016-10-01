f1c346baf75117152a7b1b58572c6598ca13a5d6
cjackychen <jzc5521@psu.edu>
2016-09-30 20:28:28 -0400

    Create displayTemp.py
    Working code for displaying temperature

---
+++ ClimateControlSystem/displayTemp.py
@@ -0,0 +1,18 @@
+import grovepi
+import math
+import decimal
+
+#Connect sensor to port D4
+
+sensor = 4 #set which port is being used
+
+While True:
+  try:
+      [temp,humidity] = grovepi.dht(sensor,1) #Checks if the sensor is in the right port
+      temp = round(temp * 9/5 + 32.1) #temperature conversion from Celsius to Fahrenheit
+      print "temp =", temp, "F/thumidity =", humidity,"%"
+      t = str(temp)
+      h = str(humidity)
+   
+   except IOError:
+      print "Error"
