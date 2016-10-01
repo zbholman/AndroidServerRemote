7b89cae90f49a064a5597d95389260b22fe932a5
plu396111 <plu396111@gmail.com>
2016-10-01 15:08:48 -0400

    Add files via upload

---
+++ SafetyAndAccessControlSystem/Car_Speed.py
@@ -0,0 +1,14 @@
+# Author: Phuong Lu
+# Date: 10/1/2016
+# Course: IST 440W, Team 6
+# Purpose: Detect car speed with accelerometer from Sense HAT
+
+from sense_hat import SenseHat
+
+sense = SenseHat()
+raw = sense.get_accelerometer_raw()
+
+print(sense.accelerometer)
+
+
+
