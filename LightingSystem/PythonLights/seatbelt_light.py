7861b900d599c0b183a5e11136aa4a5af27b7b24
arr5365 <arr5365@psu.edu>
2016-10-02 17:36:16 -0400

    added by Team04

---
+++ LightingSystem/PythonLights/seatbelt_light.py
@@ -0,0 +1,58 @@
+import time
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+
+seatbeltlight = True
+number_of_blinks = 5
+
+for x in range(0, number_of_blinks):
+    
+    sense.set_pixel(2, 7, 255, 0, 0)
+    sense.set_pixel(5, 7, 255, 0, 0)
+    sense.set_pixel(2, 6, 255, 0, 0)
+    sense.set_pixel(5, 6, 255, 0, 0)
+    sense.set_pixel(3, 6, 255, 0, 0)
+    sense.set_pixel(4, 6, 255, 0, 0)
+    sense.set_pixel(2, 5, 128, 128, 128)
+    sense.set_pixel(5, 5, 128, 128, 128)
+    sense.set_pixel(4, 5, 128, 128, 128)
+    sense.set_pixel(3, 5, 128, 128, 128)
+    sense.set_pixel(5, 4, 255, 0, 0)
+    sense.set_pixel(4, 4, 255, 0, 0)
+    sense.set_pixel(3, 4, 255, 0, 0)
+    sense.set_pixel(2, 4, 255, 0, 0)
+    sense.set_pixel(2, 3, 255, 0, 0)
+    sense.set_pixel(3, 3, 255, 0, 0)
+    sense.set_pixel(4, 3, 255, 0, 0)
+    sense.set_pixel(5, 3, 255, 0, 0)
+    sense.set_pixel(3, 2, 255, 0, 0)
+    sense.set_pixel(4, 2, 255, 0, 0)
+    sense.set_pixel(2, 1, 255, 0, 0)
+    sense.set_pixel(3, 1, 255, 0, 0)
+    sense.set_pixel(4, 1, 255, 0, 0)
+    sense.set_pixel(5, 1, 255, 0, 0)   
+    sense.set_pixel(2, 0, 255, 0, 0)
+    sense.set_pixel(3, 0, 255, 0, 0)
+    sense.set_pixel(4, 0, 255, 0, 0)
+    sense.set_pixel(5, 0, 255, 0, 0)
+    sense.set_pixel(5, 0, 255, 0, 0)
+    sense.set_pixel(5, 0, 255, 0, 0)
+    sense.set_pixel(0, 6, 128, 128, 128)
+    sense.set_pixel(1, 5, 128, 128, 128)
+    sense.set_pixel(3, 4, 128, 128, 128)
+    sense.set_pixel(4, 3, 128, 128, 128)
+    sense.set_pixel(5, 2, 128, 128, 128)
+    sense.set_pixel(6, 1, 128, 128, 128)
+    sense.set_pixel(7, 0, 128, 128, 128)
+    sense.set_pixel(6, 5, 128, 128, 128)
+    sense.set_pixel(7, 6, 128, 128, 128)
+    
+    
+    time.sleep(.5)
+    sense.clear()
+    time.sleep(.5)
+    
+    
+  
