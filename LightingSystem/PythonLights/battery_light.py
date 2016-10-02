04b3194a10f512ef104f51207674cecb3d77f79d
arr5365 <arr5365@psu.edu>
2016-10-02 16:58:38 -0400

    added by Team04

---
+++ LightingSystem/PythonLights/battery_light.py
@@ -0,0 +1,54 @@
+import time
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+
+batterylight = True
+number_of_blinks = 5
+
+for x in range(0, number_of_blinks):
+    
+    sense.set_pixel(0, 6, 255, 0, 0)
+    sense.set_pixel(1, 6, 255, 0, 0)
+    sense.set_pixel(2, 6, 255, 0, 0)
+    sense.set_pixel(3, 6, 255, 0, 0)
+    sense.set_pixel(4, 6, 255, 0, 0)
+    sense.set_pixel(5, 6, 255, 0, 0)
+    sense.set_pixel(6, 6, 255, 0, 0)
+    sense.set_pixel(7, 6, 255, 0, 0)
+    sense.set_pixel(0, 5, 255, 0, 0)
+    sense.set_pixel(0, 4, 255, 0, 0)
+    sense.set_pixel(0, 3, 255, 0, 0)
+    sense.set_pixel(0, 2, 255, 0, 0)
+    sense.set_pixel(0, 1, 255, 0, 0)
+    sense.set_pixel(1, 1, 255, 0, 0)
+    sense.set_pixel(2, 1, 255, 0, 0)
+    sense.set_pixel(3, 1, 255, 0, 0)
+    sense.set_pixel(4, 1, 255, 0, 0)
+    sense.set_pixel(5, 1, 255, 0, 0)
+    sense.set_pixel(6, 1, 255, 0, 0)
+    sense.set_pixel(7, 1, 255, 0, 0)
+    sense.set_pixel(7, 2, 255, 0, 0)
+    sense.set_pixel(7, 3, 255, 0, 0)
+    sense.set_pixel(7, 4, 255, 0, 0)
+    sense.set_pixel(7, 5, 255, 0, 0)
+    sense.set_pixel(1, 0, 255, 0, 0)
+    sense.set_pixel(2, 0, 255, 0, 0)
+    sense.set_pixel(5, 0, 255, 0, 0)
+    sense.set_pixel(6, 0, 255, 0, 0)
+    sense.set_pixel(1, 3, 255, 0, 0)
+    sense.set_pixel(2, 3, 255, 0, 0)
+    sense.set_pixel(6, 3, 255, 0, 0)
+    sense.set_pixel(5, 3, 255, 0, 0)
+    sense.set_pixel(4, 3, 255, 0, 0)
+    sense.set_pixel(5, 2, 255, 0, 0)
+    sense.set_pixel(5, 4, 255, 0, 0)
+    
+    
+    time.sleep(.5)
+    sense.clear()
+    time.sleep(.5)
+    
+    
+  
