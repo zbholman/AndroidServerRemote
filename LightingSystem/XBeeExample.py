7ce0d8eae82d0f116e8e022417d47e683e5d5680
Brian Holman <zbholman@gmail.com>
2016-10-02 20:03:41 -0400

    made a commenting script

--- LightingSystem/XBeeExample.py
+++ LightingSystem/XBeeExample.py
@@ -1,3 +1,8 @@
+# Author: Brian Holman
+# Course: IST 440
+# Team: LightingSystem
+# Date: 10-02-2016
+
 import serial
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
---
+++ comment_script.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# This script creates the comment header for python files you've created
+# Author: Brian Holman
+# Course: IST 440
+# Date: 10/02/2016
+
+### Description
+printf "\nThis script creates comment headers for your python files\n"
+printf "Hopefully it saves you some time\n\n"
+
+### Create variables
+printf "Enter your first name:\n"
+read firstName
+printf "Enter your last name:\n"
+read lastName
+printf "Enter your team directory in the git repo, ex: LightingSystem\n"
+read teamDirectory
+todaysDate=$(date +%m-%d-%Y) # Date format of MM-DD-YYYY
+
+### Create comment format
+headerComment="# Author: $firstName $lastName&\n# Course: IST 440&\n# Team: $teamDirectory&\n# Date: $todaysDate&\n&\n"
+
+### Function for adding comment if it doesn't exist
+function checkHeader() {
+
+	# Variable to check the first line of the file
+	firstLine=$(head -c 8 $1)
+
+	# If the first line starts with '# Author' skip file
+	if [[ "$firstLine" != "# Author" ]]; then
+
+		# If the first line doesn't, ask user if they created it
+		printf "$f doesn't have a header comment\n"
+		read -p "If you created it, would you like to add a header comment? (y/n)" -n 1 -r
+		echo
+
+		# If they reply 'y', then create header comment
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			
+			# If the user is on mac, use gsed command (requires homebrew and gsed)
+			if [ "$(uname)" == "Darwin" ]; then
+				# Enter header comment into the top of the file
+    			gsed -i "1s/^/$headerComment/" $1
+	
+    		# If the user is on linux, used normal sed command
+			elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+				sed -i "1s/^/$headerComment/" $1
+			fi
+		fi
+	fi
+}
+
+### Navigate to team directory
+cd $teamDirectory
+
+### Set internal file seperator to recognize spaces in filenames
+IFS=$'\n'
+
+### Find all python files in team directory, and run header function
+for f in $(find . -name '*.py'); do
+  checkHeader $f
+done
