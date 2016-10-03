c31a6251c3649dd4637a161c9e9361caebf7fceb
jol5460 <jol5460@psu.edu>
2016-10-02 22:48:39 -0400

    NFC
    NFC Python File to test out Adafruit once installed

---
+++ SafetyAndAccessControlSystem/nfc.py
@@ -0,0 +1,22 @@
+#Joey Lee
+#Version 1.00
+#10-2-16
+
+import nfc
+
+#If a tag is connected:Print out tag info
+def connected(tag): print(tag); return False
+
+#Locates and Opens contactless reader on raspberry Pi Uart
+clf = nfc.ContactlessFrontend('tty')
+
+print(clf)
+
+#Should Print Out a Certain String
+assert(clf == 'Adafruit Board on tty:USB0:pn532' )
+
+#Tests if a tag is recognized and removed
+clf.connect(rdwr={})
+
+#Starts a class method if a tag is recognized
+clf.connect(rdwr={'on-connect': connected})
