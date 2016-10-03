3565dbb92a7e403fdcd377a357c91557e45d1710
plu396111 <plu396111@gmail.com>
2016-10-03 18:41:33 -0400

    Update Car_Speed.py

--- SafetyAndAccessControlSystem/Car_Speed.py
+++ SafetyAndAccessControlSystem/Car_Speed.py
@@ -7,11 +7,14 @@ from sense_hat import SenseHat
 from doors_locked.py import doors_lock
 from doors_unlock.py import doors_unlock
 
+#Get car speed from accelerometer
 sense = SenseHat()
 raw = sense.get_accelerometer_raw()
 
+#Display speed
 print(sense.accelerometer)
 
+#Call functions to lock/unlock car if speed is over or under 5
 if sense.accelerometer >= 5:
 	door_lock()
 else sense.accelerometer < 5:
