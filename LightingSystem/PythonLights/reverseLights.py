1c0cc6de60b0d678cff90e2f60d93fb90a4ff421
jharrison357 <jqh5373@gmail.com>
2016-10-04 22:41:05 -0400

    reverse lights john

---
+++ LightingSystem/PythonLights/reverseLights.py
@@ -0,0 +1,27 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+
+reverselights = True
+
+if reverselights == True:
+    sense.set_pixel(7, 7, 128, 128, 128)
+    sense.set_pixel(7, 6, 128, 128, 128)
+    sense.set_pixel(6, 7, 128, 128, 128)
+    sense.set_pixel(6, 6, 128, 128, 128)
+    sense.set_pixel(5, 7, 128, 128, 128)
+    sense.set_pixel(5, 6, 128, 128, 128)
+    sense.set_pixel(4, 7, 128, 128, 128)
+    sense.set_pixel(4, 6, 128, 128, 128)
+    sense.set_pixel(3, 7, 128, 128, 128)
+    sense.set_pixel(3, 6, 128, 128, 128)
+    sense.set_pixel(2, 7, 128, 128, 128)
+    sense.set_pixel(2, 6, 128, 128, 128)
+    sense.set_pixel(1, 7, 128, 128, 128)
+    sense.set_pixel(1, 6, 128, 128, 128)
+    sense.set_pixel(0, 7, 128, 128, 128)
+    sense.set_pixel(0, 6, 128, 128, 128)
+
+
+
