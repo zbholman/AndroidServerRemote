cc1c574c15cb920578010726aa71fccd827ef75b
Alan <inspirah94@gmail.com>
2016-10-06 15:16:22 -0400

    Update NotificationUnitTest.py

--- ConnectivityAndNotificationSystem/NotificationUnitTest.py
+++ ConnectivityAndNotificationSystem/NotificationUnitTest.py
@@ -5,43 +5,40 @@ import hashlib
 
 class Connected_notification:
 	def ConvertFromJSON(json_string):
-    		contents = json.loads(json_string)
-    		return Message(contents['CID'], contents['OID'], contents['DID'], contents['HC'], contents['TS'], contents['TTL'], contents['PLD'], contents['UID'], contents['CKS'])
+    	contents = json.loads(json_string)
+    	return Message(contents['CID'], contents['OID'], contents['DID'], contents['HC'], contents['TS'], contents['TTL'], contents['PLD'], contents['UID'], contents['CKS'])
 	
 	def cell_connection (self,Cellular):
 		devicename=''
 		if (self.Cellular == True):
-      print('The %s is now connected to the cellular network' % devicename)
+			print('The %s is now connected to the cellular network' % devicename)
 		else:
-      print ('No Cellular device is connected at the moment')
+			print ('No Cellular device is connected at the moment')
 		return devicename
-  
 	def blue_connection (self,Bluetooth):
 		devicename_phone=''
 		if(self.Bluetooth == True):
-		  print('This %s is now connected via Bluetooth ' % devicename_phone )
+			print('This %s is now connected via Bluetooth ' % devicename_phone )
 		else:
-		  print ('No device connected. \n Please turn on bluetooth on your device.')
+			print ('No device connected. \n Please turn on bluetooth on your device.')
 		return devicename_phone
-  
-  def tms_notification(self,USB):
-    device=''
-    if (self.USB == True):
-      print('The %s is now connected through USB' % device)
-    else:
-      print ('No device is connected through USB')
-    return device
-  
-  def BatteryPackStatus(self,kwhLevel):
-    if self.kwhLevel <= LITHIUMKWHPACKLOW:
-      NOTIFYLEVEL = 'CRITICAL'
-      print (NOTIFYLEVEL)
-    elif self.kwhLevel <= LITHIUMKWHPACKMEDIUM:
-      NOTIFYLEVEL = 'HIGH'
-      print(NOTIFYLEVEL)
-    elif self.kwhLevel <= LITHIUMKWHPACKHIGH:
-      NOTIFYLEVEL = 'MEDIUM'
-      print(NOTIFYLEVEL)
-    elif self.kwhLevel <= LITHIUMKWHPACKFULL:
-      NOTIFYLEVEL = 'LOW'
-      print(NOTIFYLEVEL)
+	def tms_notification(self,USB):
+		device=''
+		if (self.USB == True):
+			print('The %s is now connected through USB' % device)
+		else:
+			print ('No device is connected through USB')
+		return device
+	def BatteryPackStatus(self,kwhLevel):
+		if self.kwhLevel <= LITHIUMKWHPACKLOW:
+			NOTIFYLEVEL = 'CRITICAL'
+			print (NOTIFYLEVEL)
+		elif self.kwhLevel <= LITHIUMKWHPACKMEDIUM:
+			NOTIFYLEVEL = 'HIGH'
+			print(NOTIFYLEVEL)
+		elif self.kwhLevel <= LITHIUMKWHPACKHIGH:
+			NOTIFYLEVEL = 'MEDIUM'
+			print(NOTIFYLEVEL)
+		elif self.kwhLevel <= LITHIUMKWHPACKFULL:
+			NOTIFYLEVEL = 'LOW'
+			print(NOTIFYLEVEL)
