e48d4423b6468755ecf19e5a534540eeddbecbc3
niravhpatel <niravhpatel1990@gmail.com>
2016-10-06 20:44:15 -0400

    updated the logging class with methods

--- MonitoringAndLoggingSystem/logging.py
+++ MonitoringAndLoggingSystem/logging.py
@@ -7,17 +7,15 @@ from pymongo import MongoClient
 
 class Log:
 	
-	conn = object
-	
 	def __init__(self):
-			#initialize db connection
+		#initialize db connection
         def get_db():
 		from pymongo import MongoClient
     		client = MongoClient('localhost:27017')
     		db = client.logging
     		return db
 	def add_log(db):
-    		db.log.insert({"ID" : "Car123"})
+    		db.log.insert({"ID" : "Car123","CID":"bks"})
 
 	def get_log(db):
     		return db.log.find_one()
@@ -31,6 +29,26 @@ class Log:
     		add_log(db)
     		print get_log(db)
 		
-	#This method stores the log in a database for future use
-	def WriteToDB(self):
+	#This method add message to log in a database for future use
+	def AddMessages():
+		pass
+
+	#This method retrievs the message by subsystems
+        def RetrieveBySubSystem():
+                pass
+
+	#This method retrieves the messages by error from log
+        def RetrieveErrors():
+                pass
+
+	#This method retrieves all the messages from log
+        def RetrieveAll():
+                pass
+	
+	#This method will clear all messages
+        def ClearAll():
+                pass
+
+	#This method will clear messages up to the date provided
+	def ClearupToDate():
 		pass
