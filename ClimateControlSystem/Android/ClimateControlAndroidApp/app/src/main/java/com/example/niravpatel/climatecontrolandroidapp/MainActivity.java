f683b3b7ed8034afc4356c71a6456d96a3c5bbaf
Your Name <niravhpatel1990@gmail.com>
2016-10-10 20:16:11 -0400

    Updated Android UI by adding widgets

--- ClimateControlSystem/Android/ClimateControlAndroidApp/app/src/main/java/com/example/niravpatel/climatecontrolandroidapp/MainActivity.java
+++ ClimateControlSystem/Android/ClimateControlAndroidApp/app/src/main/java/com/example/niravpatel/climatecontrolandroidapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.niravpatel.climatecontrolandroidapp;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 import android.widget.Switch;
@@ -13,23 +16,24 @@ import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ToggleButton btnToggleAC;
+    private ToggleButton btnToggleAC,btnToggleHeat;
+    private Switch switchFan;
+    ImageButton btntemp1Inc,btntemp1Dec,btntemp2Inc,btntemp2Dec;
+    TextView temp1,temp2;
+    int tempCounter1 , tempCounter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // initiate a Switch
-        Switch simpleSwitch = (Switch) findViewById(R.id.switchAuto);
-
-        // check current state of a Switch (true or false).
-        Boolean switchState = simpleSwitch.isChecked();
-
-        simpleSwitch.setTextOn("On"); // displayed text of the Switch whenever it is in checked or on state
-        simpleSwitch.setTextOff("Off"); // displayed text of the Switch whenever it is in unchecked i.e. off state
-
         btnToggleAC = (ToggleButton) findViewById(R.id.btnToggleAC);
+        btnToggleHeat = (ToggleButton) findViewById(R.id.btnToggleHeat);
+        btntemp1Inc = (ImageButton) findViewById(R.id.btnTemp1Inc);
+        btntemp1Dec = (ImageButton) findViewById(R.id.btnTemp1Dec);
+
+        switchFan = (Switch) findViewById(R.id.switchFan);
+
         btnToggleAC.setOnClickListener(new OnClickListener() {
 
             @Override
@@ -43,5 +47,29 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btnToggleHeat.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer result = new StringBuffer();
+                result.append("Heat : ").append(btnToggleHeat.getText());
+
+                Toast.makeText(MainActivity.this, result.toString(),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btntemp1Inc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                tempCounter1 ++;
+                temp1.setText(tempCounter1);
+
+            }
+        });
     }
-    }
+}
