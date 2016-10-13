89a129546ce6130a99db17198d5e534b9c6df548
sxp5276 <sxp5276@psu.edu>
2016-10-13 11:14:20 -0400

    Update Speaker_On.py

--- SafetyAndAccessControlSystem/Speaker_On.py
+++ SafetyAndAccessControlSystem/Speaker_On.py
@@ -21,17 +21,12 @@ e,e,b,b,e,b,e,e,
 e,e,e,b,b,e,e,e
 ]
 
-sense.set_pixels(image)
-time.sleep(3)
-
-sense.show_message("Speaker On")
-images = [speaker_on]
 count = 0
 i=0
 while (i<3): 
-   sense.set_pixels(images[count % len(images)]())
+   sense.set_pixels(image)
    time.sleep(.75)
    sense.clear()
    time.sleep(.75)
-   sense.set_pixels(images[count % len(images)]())
+   sense.set_pixels(image)
    i+=1
