fe82ec5847fd503ed95bdc79317f51dc6929a848
niravhpatel <niravhpatel1990@gmail.com>
2016-10-18 01:46:46 +0000

    updated display temperature

--- ClimateControlSystem/Python/displayTemp.py
+++ ClimateControlSystem/Python/displayTemp.py
@@ -1,18 +1,41 @@
-import grovepi
-import math
-import decimal
+from grovepi import grovepi
+import os
+import time
 
-#Connect sensor to port D4
+# --------- User Settings ---------
+# The DHT_SENSOR_TYPE below may need to be changed depending on which DHT sensor you have:
+#  0 - DHT11 - blue one - comes with the GrovePi+ Starter Kit
+#  1 - DHT22 - white one, aka DHT Pro or AM2302
+#  2 - DHT21 - black one, aka AM2301
+DHT_SENSOR_TYPE = 1
+# Connect the DHT sensor to one of the digital pins (i.e. 2, 3, 4, 7, or 8)
+DHT_SENSOR_PIN = 4
+CONVERT_TO_FAHRENHEIT = True
+# ---------------------------------
 
-sensor = 4 #set which port is being used
+def isFloat(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
 
 while True:
-  try:
-      [temp,humidity] = grovepi.dht(sensor,1) #Checks if the sensor is in the right port
-      temp = round(temp * 9/5 + 32.1) #temperature conversion from Celsius to Fahrenheit
-      print "temp =", temp, "F/thumidity =", humidity,"%"
-      t = str(temp)
-      h = str(humidity)
+    try:
+        [temp_c,hum] = grovepi.dht(DHT_SENSOR_PIN,DHT_SENSOR_TYPE)
+        if isFloat(temp_c):
+                if (CONVERT_TO_FAHRENHEIT):
+                        temp_f = temp_c * 9.0 / 5.0 + 32.0
+                        print "Temperature(F):", temp_f
+                else:
+                        print "Temperature(C):", temp_c
 
-  except IOError:
-        print "Error"
+        if ((isFloat(hum)) and (hum >= 0)):
+                print "Humidity(%):", hum
+
+    except IOError:
+        print ("Error")
+
+    time.sleep(5)
+
