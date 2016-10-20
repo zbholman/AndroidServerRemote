fb99535897fdd0821b3539d4dcb420b90bd48396
ElaineTang <eot5104@gmail.com>
2016-10-20 19:53:41 -0400

    Added by Elaine

---
+++ CommandAndControlSystem/Queuing/receiveLS.py
@@ -0,0 +1,22 @@
+
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='Lighting System received', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue='Lighting System received')
+
+channel.start_consuming()
