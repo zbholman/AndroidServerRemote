be423ec404b17492c9f3ecd77d824e3e8988370c
Keval Patel <kpp5104@psu.edu>
2016-10-20 17:15:45 -0400

    Update bluetooth.sh

--- ConnectivityAndNotificationSystem/bluetooth.sh
+++ ConnectivityAndNotificationSystem/bluetooth.sh
@@ -58,6 +58,19 @@ cat /etc/group | grep bluetooth
 	fi
 
 
+
+# to check the raspberry pi bluetooth name
+sudo hciconfig hci0 name
+
+# to set the bluetooth name
+sudo hciconfig hci0 name raspberrypi
+
+# make the raspberry pi discoverable
+sudo hciconfig hci0 piscan
+
+# setup agent to listen for pairing request
+sudo bluetooth-agent 1234
+
 # package required for 'pand' command
 sudo apt-get install bluez-compat
 
