255005113ceab6d897587e93c900ac38cad18bfb
ElaineTang <eot5104@gmail.com>
2016-10-20 20:40:17 -0400

    Added by Elaine

--- CommandAndControlSystem/Queuing/receiveLS.py
+++ CommandAndControlSystem/Queuing/receiveLS.py
@@ -12,7 +12,7 @@ print(' [*] Waiting for messages. To exit press CTRL+C')
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     time.sleep(body.count(b'.'))
-    print(" [x] Done")
+    print(" [x] Lighting system received")
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
