46f70a34d727b1380ae5f7a69c532b984c14f7a2
jharrison357 <jqh5373@gmail.com>
2016-10-22 18:13:44 +0000

    phat lights by john

--- LightingSystem/PythonLights/Scroll_phat/high_beams.py
+++ LightingSystem/PythonLights/Scroll_phat/high_beams.py
@@ -5,10 +5,13 @@ import scrollphat
 
 scrollphat.set_brightness(5000)
 
+
 for y in range(5):
     for x in range(11):
         scrollphat.set_pixel(x,y,True)
         scrollphat.update()
+
+    
     
 
 
