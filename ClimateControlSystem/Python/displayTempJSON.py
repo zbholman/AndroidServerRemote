77f2b9aa75c1323feceb369ed526a5027e11f821
MoAmmouni <mohammedammouni@gmail.com>
2016-10-25 11:12:35 -0400

    Update displayTempJSON.py

--- ClimateControlSystem/Python/displayTempJSON.py
+++ ClimateControlSystem/Python/displayTempJSON.py
@@ -33,10 +33,12 @@ while True:
 
         if ((isFloat(hum)) and (hum >= 0)):
                 print "Humidity(%):", hum
-    tempTOjson = {'temp': t, 'humidity': h}
+                
+# Create a dictionary object for gathering all data from temperature sensor to write out to a JSON file
+    tempTOjson = {'temp': temp_f, 'humidity': hum}
 
-    fileName = 'displayTemp.json'#name of the python code
-    outFile = open(fileName, 'w')#W stands for writing
+    fileName = 'displayTemp.json'# name of the JSON output file
+    outFile = open(fileName, 'w')#W stands for writing, writing out to JSON file
     json.dump(tempTOjson, outFile)#Dumping all contents from Temp to Json
     outFile.close()#Close the outfile
     except IOError:
