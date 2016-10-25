f0ab758ec9ad4c99927ff1a2a50002d0d838292a
Nirav Patel <niravhpatel1990@gmail.com>
2016-10-24 23:52:02 -0400

    testing Auto FanOn and Off based on temp

--- ClimateControlSystem/Python/FanOnOff.py
+++ ClimateControlSystem/Python/FanOnOff.py
@@ -4,19 +4,16 @@ import time
 import datetime
 import sys
  
-# 5 * * * * sudo python /home/pi/fan.py
-# A crontab will run every hour and check the temp. If the temp is > 49 the script will start the fan
-# until the temperature goes down to 28. When it does, the script will end, shutting down the fan as well.
-# If the script executes again while a previous script is running, the latter will exit
-# ... meaning the pi is in hell, and will never get bellow FAN_END value :P
+#If the temp is > 65 the script will start the fan until the temperature goes down to 66. When it does, the script will end,
+# shutting down the fan as well. If the script executes again while a previous script is running, the latter will exit.
  
 # Identify which pin controls the relay
 FAN_PIN = 4
 # the yellow box ex: GPIO18
 # Temperature check. Start fan if temp > 49C
-FAN_START = 85
+FAN_START = 65
 # Temperature check. Shut down under 28C
-FAN_END = 84
+FAN_END = 66
 
 DHT_SENSOR_TYPE = 1
 # Connect the DHT sensor to one of the digital pins (i.e. 2, 3, 4, 7, or 8)
@@ -64,30 +61,26 @@ def check_fan(pin):
         return GPIO.input(pin)
  
 def run(pin):
-        current_date = datetime.datetime.now()
         temp = get_temp_from_system()
         if float(temp) >= FAN_START:
-                print(temp+' @ '+str(current_date))
                 if check_fan(pin) == 1:
                         print('Fan is Off...Starting Fan')
                         fanON()
                 else:
-                        time.sleep(5) # Remove, if you want real-time checking
                         print('Fan is ON')
         elif float(temp) <= FAN_END:
-                print(temp+' @ '+str(current_date))
                 if check_fan(pin) == 0:
                         print('Fan is on...Shuting it Down')
                         fanOFF()
                         GPIO.cleanup()
-                        return 1 # exit script. The pi has cooled down
+                        return 1 # exit script. 
                 else:
                         time.sleep(5) # Remove, if you want real-time checking
                         print('Fan is OFF')
         else:
                         pass # while the script is passing through here, there will be no output on screen
                        
-                       
+#testing command line                        
 if action == "on" :
    print "Turning fan on"
    fanON()
