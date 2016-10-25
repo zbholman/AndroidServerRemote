b4587ea03e735b146d04139002fa6d23becee209
sxp5276 <sxp5276@psu.edu>
2016-10-25 11:39:48 -0400

    Update music.py

--- SafetyAndAccessControlSystem/music.py
+++ SafetyAndAccessControlSystem/music.py
@@ -1,9 +1,17 @@
 #Shivang Patel
 #10/12/2016
+#IST 440W
+#PENNSTATE ABINGTON
+#Prof Joe Oakes
 #python code for playing sound/music.
 
 import pygame
 
+#	initialize the mixer module
 pygame.mixer.init()
+
+#Load a music file forever.mp3 for playback
 pygame.mixer.music.load("Forever.mp3")
+
+#Start the playback of the music stream
 pygame.mixer.music.play()
