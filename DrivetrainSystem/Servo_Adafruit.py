50fbb025fceed85a5dd29d9cf1cb5fd3e2f51a9f
ggp5014 <ggp5014@psu.edu>
2016-10-25 10:55:19 -0400

    Update Servo_Adafruit.py

--- DrivetrainSystem/Servo_Adafruit.py
+++ DrivetrainSystem/Servo_Adafruit.py
@@ -9,11 +9,6 @@ import time
 # Import the PCA9685 module.
 import Adafruit_PCA9685
 
-
-# Uncomment to enable debug output.
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
-
 # Initialise the PCA9685 using the default address (0x40).
 pwm = Adafruit_PCA9685.PCA9685()
 
@@ -39,6 +34,7 @@ def set_servo_pulse(channel, pulse):
 # Set frequency to 60hz, for servos.
 pwm.set_pwm_freq(60)
 
+# To quit
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:
     # Move servo on channel O between extremes.
