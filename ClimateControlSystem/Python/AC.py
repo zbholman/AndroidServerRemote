e0a6d15da899dfae202962b0cc0cf0823a0ba56e
niravhpatel <niravhpatel1990@gmail.com>
2016-10-25 03:44:51 +0000

    Added code for AC cooler

---
+++ ClimateControlSystem/Python/AC.py
@@ -0,0 +1,45 @@
+# AC.py
+# IST440 Team 3
+# author: Nirav
+
+#import statements
+
+import os
+import RPi.GPIO as GPIO
+import time
+import datetime
+import sys
+
+# GPIO pin on raspberry Pi to connect relay pin(Fan connected to relay One)
+
+AC_PIN = 4
+
+# Method to setup GPIO pins.
+def GPIOsetup():
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(AC_PIN, GPIO.OUT)
+
+# Method to turn relay ON that is Ac ON
+def AcON():
+        GPIOsetup()
+        GPIO.output(AC_PIN, 0) #Ac On
+        return()
+
+# Method to tur relay OFF that is Ac OFF
+def AcOFF():
+        GPIOsetup()
+        GPIO.output(AC_PIN, 1) #Ac off
+        return()
+
+#While loop to keep Fan On until user end loop using Ctrl + Z
+while True:
+        try:
+                AcON()
+        except KeyboardInterrupt:
+                print "  Quit"
+
+  # Reset GPIO settings
+GPIO.cleanup()
+
+
