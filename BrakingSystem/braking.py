5d1c82c0fb56e2afae91a896856ae03d567fb98c
David Austin <david.austin89@gmail.com>
2016-10-25 11:54:00 -0400

    Update braking.py
    Added Intro Comments and edited if statement

--- BrakingSystem/braking.py
+++ BrakingSystem/braking.py
@@ -1,11 +1,17 @@
+#Penn State Abington
+#IST 440W
+#Fall 2016
+#Team Pump Your Brakes
+#Members: Abu Sakif, David Austin, Qili Jian, Abu Chowdhury, Gary Martorana, Chakman Fung
+
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(11,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)#
 
-p = GPIO.PWM(11,50)#puts pin 11 to 50Hz
+p = GPIO.PWM(11,50)#PWM'Post-width Modulation' puts pin 11 to 50Hz
 p.start(9)
 
 loopStart =  time.time()#time when program starts
@@ -16,9 +22,10 @@ try:
                 timePassed = now - loopStart#current time minus time when program started
                 timePassed = int(timePassed)#makes timePassed into integer
                 p.ChangeDutyCycle(13.5)#engage brake
-                if timePassed >= 2:
-                        p.ChangeDutyCycle(9)#disengage brakes after 2 seconds
-                        p.stop()#stops power to servo
+        if timePassed >= 2:
+                p.ChangeDutyCycle(9)#disengage brakes after 2 seconds
+                p.stop()#stops power to servo
+                GPIO.cleanup()
 
 except KeyboardInterrupt:
         GPIO.cleanup()
