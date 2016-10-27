c716434acdc723d4a6078350c1ade672f1cb935f
Nirav Patel <niravhpatel1990@gmail.com>
2016-10-26 23:28:55 -0400

    Added New UserInterface for Climate Control

---
+++ ClimateControlSystem/Android/ClimateControl/app/src/main/java/climatecontrol/ist440/climatecontrol/MainActivity.java
@@ -0,0 +1,13 @@
+package climatecontrol.ist440.climatecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
