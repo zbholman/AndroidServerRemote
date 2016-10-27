3a31d7efb68aa1f1b477b4a7f4862527df298a4f
ivs5069 <ivs5069@gmail.com>
2016-10-27 00:54:06 -0400

    Adding sorting of messages to put them into different collections

--- MonitoringAndLoggingSystem/logging.py
+++ MonitoringAndLoggingSystem/logging.py
@@ -14,11 +14,45 @@ class Log:
                 client = MongoClient('localhost:27017')
                 db = client.dbNiravkumar
                 return db
+	
+	#Method that determines what collections to put the message into
+	#and puts that message into the collection.
+	#Sorts it by HealthCode, Origin, and puts all messages into a default collection
+	def Parse_Message_To_Collection(inc_message):
+		
+		#If the payload contains a healthcode, put it in the errors collection
+        	if(inc_message.Return_Payload()[:2:] == "HC"):
+                	add_message(inc_message, db.errors)
 
-        def add_message(db, message):
-               
-		db.logging.insert(message.ConvertToDictionary)
-		 #db.logging.insert({"PLD": "payload: This message format defined by subteams", "UID": "cc077cc47be14d968fc1947e7edd3436", "CKS": "8fcffdcd1c1dbd62c199aa2a13c9043a", "CID": "car10393", "DID": "ML", "HC": 3, "TTL": 100, "OID": "CC", "TS": 1476407469.717848})
+		#Put the message into the collection dependant on where the message originated from
+        	if(inc_message.Return_Origin_ID() == "brs"):
+                	add_message(inc_message, db.brs)                                                              
+        	elif(inc_message.Return_Origin_ID() == "clc"):
+                	add_message(inc_message, db.clc)
+       		elif(inc_message.Return_Origin_ID() == "ems"):
+                	add_message(inc_message, db.ems)
+        	elif(inc_message.Return_Origin_ID() == "ccs"):
+                	add_message(inc_message, db.ccs) 
+        	elif(inc_message.Return_Origin_ID() == "mls"):
+                	add_message(inc_message, db.mls)
+        	elif(inc_message.Return_Origin_ID() == "cns"):
+                	add_message(inc_message, db.cns)
+        	elif(inc_message.Return_Origin_ID() == "dts"):
+                	add_message(inc_message, db.dts)
+        	elif(inc_message.Return_Origin_ID() == "lis"):
+                	add_message(inc_message, db.lis)
+        	elif(inc_message.Return_Origin_ID() == "sac"):
+                	add_message(inc_message, db.sac)
+		
+		#Put all of the messages into one central default collection
+		add_message(inc_message)
+
+
+	#Method to add a message to the mongo database
+        def add_message(message, collection):
+		collection.insert(message.Convert_To_Dictionary())
+	
+
 	#returns a message from the current database, collection 'logging'
         def get_message(db):
                 return db.logging.find_one()
