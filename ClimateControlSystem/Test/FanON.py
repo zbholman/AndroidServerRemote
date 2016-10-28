1875fa92886d373bcfe19e4b4c0e85ed70ed1432
procom90 <procom22@gmail.com>
2016-10-27 21:51:11 -0400

    Update FanON.py

--- ClimateControlSystem/Test/FanON.py
+++ ClimateControlSystem/Test/FanON.py
@@ -1,27 +1,19 @@
+#IST440
+#Climate Control
 #Ahmad Alhaddad
+#10/23/2016
 #Fan ON
 
-
-
+#General Purpose input/output
 def GPIOsetup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(FAN_PIN, GPIO.OUT)
     GPIO.setwarnings(False)
-
-def fanON(): #
-    GPIO.output(FAN_PIN, 0)
-    print("fan on")
+#Turns the fan on 
+def fanON() -> object: #
+    GPIO.output(FAN_PIN, 0)#Pin means fan is on 
+    print("fan on")#It displays that the fan is on
     return()
 
-def main():
-    GPIOsetup()
-    getTEMP()
-
-try:
-    main()
-finally:
-    print ("Finish")
-    #GPIO.cleanup()
-
+#turns fan on
 fanON()
-
