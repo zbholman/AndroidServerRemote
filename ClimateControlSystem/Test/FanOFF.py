d450ce48c58a59d9dc9fd9b4fa996f3480f129f0
procom90 <procom22@gmail.com>
2016-10-27 21:51:47 -0400

    Update FanOFF.py

--- ClimateControlSystem/Test/FanOFF.py
+++ ClimateControlSystem/Test/FanOFF.py
@@ -1,27 +1,20 @@
+#IST440
+#Climate Control
 #Ahmad Alhaddad
+#10/23/2016
 #Fan OFF
 
 
-
+#General Purpose input/output
 def GPIOsetup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(FAN_PIN, GPIO.OUT)
     GPIO.setwarnings(False)
-
+#Turns the fan off
 def fanOFF():
-    GPIO.output(FAN_PIN, 1)
-    print("fan off")
+    GPIO.output(FAN_PIN, 1) #Pin means fan is off
+    print("fan off") #It displays that the fan is off
     return()
 
-def main():
-    GPIOsetup()
-    getTEMP()
-
-try:
-    main()
-finally:
-    print ("Finish")
-    #GPIO.cleanup()
-
+#turns fan off
 fanOFF()
-
