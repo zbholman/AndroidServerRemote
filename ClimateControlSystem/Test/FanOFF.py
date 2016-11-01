7743f44fbdf67aa727fa02bf72c2973b1e24bf1f
procom90 <procom22@gmail.com>
2016-11-01 11:52:55 -0400

    Update FanOFF.py

--- ClimateControlSystem/Test/FanOFF.py
+++ ClimateControlSystem/Test/FanOFF.py
@@ -1,18 +1,26 @@
 #IST440
 #Climate Control
-#Ahmad Alhaddad
+#Ahmad Alhaddad/Yusef Savage
 #10/23/2016
 #Fan OFF
 
+#import statements
 
-#General Purpose input/output
+import RPi.GPIO as GPIO
+import time
+
+# GPIO pin on raspberry Pi to connect relay pin(Fan connected to relay One)
+
+FAN_PIN = 4
+
+#General Purpose input/output 
 def GPIOsetup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(FAN_PIN, GPIO.OUT)
     GPIO.setwarnings(False)
-#Turns the fan off
+#Turns the fan off 
 def fanOFF():
-    GPIO.output(FAN_PIN, 1) #Pin means fan is off
+    GPIO.output(FAN_PIN, 1) #Pin means fan is off 
     print("fan off") #It displays that the fan is off
     return()
 
