ea6581980c4a3e689d593b71a6f45edd5f82bfa5
bth5132 <bth5132@psu.edu>
2016-11-01 12:18:15 -0400

    updated led bar file by defining getVoltage and importing new libraries

--- EnergyManagementSystem/voltage_led_bar.py
+++ EnergyManagementSystem/voltage_led_bar.py
@@ -7,6 +7,16 @@
 import math
 import time
 import grovepi
+import RPi.GPIO
+import smbus
+
+def getVoltage(self, input=0):
+        if input == 1:
+                return self.readWord(0b00000101)
+        elif input == 2:
+                return self.readWord(0b00000110)
+        else:
+                return self.readWord(0b00000001)
 
 LipoBattery = 5000 # mAh of lipo battery for the car
 CurrentVoltage = (getVoltage) # takes value from currently running MoPi file (still have to work out how to get them to communicate)
@@ -15,35 +25,35 @@ grovepi.pinMode(ledbar,"OUTPUT") # establish the LED bar as the output from the 
 time.sleep(1)
 i = 0
 
-if CurrentVoltage <= 11200:
+if CurrentVoltage <= 8400:
 	for i in range (0,11):
 		grovepi.ledBar_init(ledbar, 1) # initialize the entire LED bar to show full charge
-else CurrentVoltage < 10880:
+elif CurrentVoltage < 8160:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off top-most LED to show <90% charge left
-else CurrentVoltage < 10560:
+elif CurrentVoltage < 7920:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off two top-most LEDs to show <80% charge left
 		grovepi.ledBar_setLed(ledbar, 9, 0)
-else CurrentVoltage < 10240:
+elif CurrentVoltage < 7680:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off three top-most LEDs to show <70% charge left
 		grovepi.ledBar_setLed(ledbar, 9, 0)
 		grovepi.ledBar_setLed(ledbar, 8, 0)
-else CurrentVoltage < 9920:
+elif CurrentVoltage < 7440:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off four top-most LEDs to show <60% charge left
 		grovepi.ledBar_setLed(ledbar, 9, 0)
 		grovepi.ledBar_setLed(ledbar, 8, 0)
 		grovepi.ledBar_setLed(ledbar, 7, 0)
-else CurrentVoltage < 9600:
+elif CurrentVoltage < 7200:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off five top-most LEDs to show <50% charge left
 		grovepi.ledBar_setLed(ledbar, 9, 0)
 		grovepi.ledBar_setLed(ledbar, 8, 0)
 		grovepi.ledBar_setLed(ledbar, 7, 0)
 		grovepi.ledBar_setLed(ledbar, 6, 0)
-else CurrentVoltage < 9280:
+elif CurrentVoltage < 6960:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off six top-most LEDs to show <40% charge left
 		grovepi.ledBar_setLed(ledbar, 9, 0)
@@ -51,7 +61,7 @@ else CurrentVoltage < 9280:
 		grovepi.ledBar_setLed(ledbar, 7, 0)
 		grovepi.ledBar_setLed(ledbar, 6, 0)
 		grovepi.ledBar_setLed(ledbar, 5, 0)
-else CurrentVoltage < 8960:
+elif CurrentVoltage < 6720:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off seven top-most LEDs to show <30% charge left
 		grovepi.ledBar_setLed(ledbar, 9, 0)
@@ -60,7 +70,7 @@ else CurrentVoltage < 8960:
 		grovepi.ledBar_setLed(ledbar, 6, 0)
 		grovepi.ledBar_setLed(ledbar, 5, 0)
 		grovepi.ledBar_setLed(ledbar, 4, 0)
-else CurrentVoltage < 8640:
+elif CurrentVoltage < 6480:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off eight top-most LEDs to show <20% charge left
 		grovepi.ledBar_setLed(ledbar, 9, 0)
@@ -70,7 +80,7 @@ else CurrentVoltage < 8640:
 		grovepi.ledBar_setLed(ledbar, 5, 0)
 		grovepi.ledBar_setLed(ledbar, 4, 0)
 		grovepi.ledBar_setLed(ledbar, 3, 0)
-else CurrentVoltage < 8320:
+elif CurrentVoltage < 6240:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off nine top-most LEDs to show <10% charge left
 		grovepi.ledBar_setLed(ledbar, 9, 0)
@@ -81,7 +91,7 @@ else CurrentVoltage < 8320:
 		grovepi.ledBar_setLed(ledbar, 4, 0)
 		grovepi.ledBar_setLed(ledbar, 3, 0)
 		grovepi.ledBar_setLed(ledbar, 2, 0)
-else CurrentVoltage < 8000:
+else:
 	for i in range (0,11):
 		grovepi.ledBar_setLed(ledbar, 10, 0) # turn off all LEDs to show 0% charge left
 		grovepi.ledBar_setLed(ledbar, 9, 0)
@@ -94,3 +104,4 @@ else CurrentVoltage < 8000:
 		grovepi.ledBar_setLed(ledbar, 2, 0)
 		grovepi.ledBar_setLed(ledbar, 1, 0)
 
+print "Current voltage reading is: " + CurrentVoltage + ".";
