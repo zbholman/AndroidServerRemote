603f761355bacc6e519316e573b6a91f472dc684
procom90 <procom22@gmail.com>
2016-11-01 11:50:47 -0400

    Update FanON.py

--- ClimateControlSystem/Test/FanON.py
+++ ClimateControlSystem/Test/FanON.py
@@ -1,8 +1,14 @@
 #IST440
 #Climate Control
-#Ahmad Alhaddad
+#Ahmad Alhaddad/ Yusef Savage
 #10/23/2016
 #Fan ON
+
+import RPi.GPIO as GPIO
+import time
+# GPIO pin on raspberry Pi to connect relay pin(Fan connected to relay One)
+
+FAN_PIN = 4
 
 #General Purpose input/output
 def GPIOsetup():
@@ -10,7 +16,7 @@ def GPIOsetup():
     GPIO.setup(FAN_PIN, GPIO.OUT)
     GPIO.setwarnings(False)
 #Turns the fan on 
-def fanON() -> object: #
+def fanON(): #
     GPIO.output(FAN_PIN, 0)#Pin means fan is on 
     print("fan on")#It displays that the fan is on
     return()
