adf8c4d7b54be74f7ec7284c791205264bf5d953
HaoyangLuo <hyangluo@gmail.com>
2016-10-31 20:51:00 -0400

    update airbag symbol in light method
    removed unnecessary lines, added comments, and create airbag symbol

--- LightingSystem/PythonLights/light_method.py
+++ LightingSystem/PythonLights/light_method.py
@@ -1,9 +1,12 @@
+#Jigar, Haoyang
 from sense_hat import SenseHat
 import time
 s = SenseHat()
-s.low_light = True
-nothing = (0,0,0)
-red = (255, 0, 0)
+nothing = (0,0,0) # add color codes (0,0,0) to show light-off
+red = (255, 0, 0) 
+green = (0, 255, 0)
+white = (255, 255, 255)
+yellow = (255, 255, 0)
 def hazrdLight():
     R = red
     O = nothing
@@ -22,7 +25,6 @@ def hazrdLight():
 def r_signal():
     W = red
     O = nothing
-
     logo = [
     O, O, O, W, W, O, O, O,
     O, O, O, O, W, W, O, O,
@@ -65,9 +67,26 @@ def running_light():
     ]
     return logo
 
-images = [hazrdLight, r_signal, l_signal, running_light]
+def airbag_symbol():
+    R = red
+    W = white
+    Y = yellow
+    O = nothing
+    logo = [
+    O, R, R, O, O, Y, Y, O,
+    R, R, R, R, O, Y, Y, W,
+    R, R, R, R, O, R, W, O,
+    O, R, R, O, O, R, R, O,
+    O, O, O, O, W, R, R, O,
+    O, O, R, R, R, W, O, O,
+    O, R, R, R, O, O, W, O,
+    R, R, O, O, O, O, O, W,    
+    ]
+    return logo
+
+images = [hazrdLight, r_signal, l_signal, running_light, airbag_symbol] #organized all lights
 count = 0
-while True:
+while True: #display the images by sequence of the images when true
     s.set_pixels(images[count % len(images)]())
     time.sleep(.8)
     s.clear()
