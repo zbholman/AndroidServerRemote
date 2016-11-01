4350b882f79d291ae7da6bc31a5f3aec384e3cac
kbh5094 <kbh5094@psu.edu>
2016-11-01 11:54:09 -0400

    Update IST440W Driveshift.py
    Changed some code for speed, added speed up and down, working for changing between drive and reverse WIP

--- DrivetrainSystem/IST440W Driveshift.py
+++ DrivetrainSystem/IST440W Driveshift.py
@@ -3,7 +3,7 @@
 #Fall 2016
 #DriveTrain
 #Author: Klaus Herchenroder
-#Version: 1
+#Version: 2
 
 
 from __future__ import division
@@ -18,7 +18,9 @@ pwm = Adafruit_PCA9685.PCA9685()
 # Configure min and max servo pulse lengths
 servo_min = 150  # Min pulse length out of 4096 
 servo_max = 600  # Max pulse length out of 4096
-servo_neutral = 375
+servo_neutral = 375 #The max pulse length of neutal
+servo_drive = 375 #The value to be changed for speed
+
 
 # Helper function to make setting a servo pulse width simpler
 def set_servo_pulse(channel, pulse):
@@ -47,24 +49,41 @@ while True:
     if(mode == 'p'): #Sets car into park, place holder code
 	    print('Stop')
 	    
-    elif(mode == 's'): #Sets car into reverse at top speed
-	    pwm.set_pwm(0, 0, servo_min)
+    elif(mode == 's'): #Sets car into reverse
+	    pwm.set_pwm(0, 0, servo_drive)
 	    
-    elif(mode == 'w'): #Sets car into forward at top speed
-	    pwm.set_pwm(0, 0, servo_max)
+    elif(mode == 'w'): #Sets car into drive
+	    if servo_drive < 359:
+		    servo_drive = servo_drive
+	    pwm.set_pwm(0, 0, servo_drive)
 	    
     elif(mode == 'n'): #Sets car into neutral 
-	    pwm.set_pwm(0, 0, servo_neural)
+	    pwm.set_pwm(0, 0, 375)
+	    
+    elif(mode == 't'): #Speeds up the car
+	    if servo_drive <= 375 && servo_drive > 359:
+		    servo_drive = 375
+	    servo_drive = servo_drive + 10
+	    pwm.set_pwm(0, 0, servo_drive)
+
+    elif(mode == 'g'): #Speeds down the car
+	    if servo_drive < 375 && servo_drive >= 359:
+		    servo_drive = 359
+	    servo_drive = servo_drive - 10
+	    pwm.set_pwm(0, 0, servo_drive)
+
+    elif(mode == 'f'):
+	    if servo_drive != 375:
+		    servo_drive = 375
 
     elif(mode == 'q'): #Exits gear shift
 	    pwm.set_pwm(0, 0, neutral)
-	    stop()
 	    sys.exit()
+	    
     else: #If invalid command is entered
 	    print('No Command try again')
 	    time.sleep(0.1)
-
-	    
+  
     # Move servo on channel O between extremes.
     #pwm.set_pwm(0, 0, servo_min) # Servo will be on position 0
     #time.sleep(1) # 1 Second
