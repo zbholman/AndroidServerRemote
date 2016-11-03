3b14b1abf0b47277c0df985eea0f0f6ff088f407
procom90 <procom22@gmail.com>
2016-11-03 12:00:25 -0400

    Add files via upload

---
+++ ClimateControlSystem/Python/FanOFF2.py
@@ -0,0 +1,43 @@
+#IST440
+#Climate Control
+#Ahmad Alhaddad/ Yusef Savage
+#10/23/2016
+#Fan OFF
+#import required Python libraries 
+ import RPi.GPIO as GPIO 
+ import time 
+ 
+ 
+ # Use BCM GPIO references instead of physical pin numbers 
+ GPIO.setmode(GPIO.BCM) 
+ 
+ 
+ # init list with pin numbers 
+ 
+ 
+ pinList = [24] 
+ 
+ 
+ # loop through pins and set mode and state to 'low' 
+ 
+ 
+ for i in pinList:  
+     GPIO.setwarnings(False) 
+     GPIO.setup(i, GPIO.OUT)  
+     GPIO.output(i,1) 
+ 
+ 
+ def trigger() : 
+         for i in pinList:  
+           GPIO.output(i, 1) 
+ #         GPIO.cleanup() 
+           break 
+       
+ 
+ 
+ try:  
+     trigger() 
+           
+        
+ except KeyboardInterrupt: 
+   print "  Quit"  
