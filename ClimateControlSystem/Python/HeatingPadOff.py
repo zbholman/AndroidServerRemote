5f3480465f9436b8941a343636c82f6fe975d620
niravhpatel <niravhpatel1990@gmail.com>
2016-11-04 17:00:15 +0000

    Added files to turn on and off equipment

---
+++ ClimateControlSystem/Python/HeatingPadOff.py
@@ -0,0 +1,33 @@
+#import required Python libraries
+import RPi.GPIO as GPIO
+import time
+
+# Use BCM GPIO references instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# init list with pin numbers
+
+pinList = [27]
+
+# loop through pins and set mode and state to 'low'
+
+for i in pinList: 
+    GPIO.setwarnings(False)
+    GPIO.setup(i, GPIO.OUT) 
+    GPIO.output(i,1)
+
+def trigger() :
+        for i in pinList: 
+          GPIO.output(i, 1)
+#         GPIO.cleanup()
+          break
+     
+
+try: 
+    trigger()
+         
+      
+except KeyboardInterrupt:
+  print "  Quit" 
+  # Reset GPIO settings
+  GPIO.cleanup()
