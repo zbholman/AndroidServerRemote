97e1e7311bda088340e8917173d635d59047e00b
niravhpatel <niravhpatel1990@gmail.com>
2016-11-04 20:34:40 +0000

    added testing files for  Bluetooth

--- ClimateControlSystem/Python/outsideWeatherBluetoothJSON.py
+++ ClimateControlSystem/Python/outsideWeatherBluetoothJSON.py
@@ -1,8 +1,9 @@
 import json
 import pyowm #Python Open Weather Map
+import bluetooth
 
-server_sock= BluetoothSocket( RFCOMM ) 
-server_sock.bind(("",PORT_ANY)) 
+server_sock= bluetooth.BluetoothSocket(bluetooth.RFCOMM ) 
+server_sock.bind(("",1)) 
 server_sock.listen(1) 
  
  
@@ -11,10 +12,10 @@ port = server_sock.getsockname()[1]
  
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_sock, "SampleServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ])
+# advertise_service( server_sock, "SampleServer",
+#                   service_id = uuid,
+#                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+#                   profiles = [ SERIAL_PORT_PROFILE ])
 
 try:
 # Register API Key with source
