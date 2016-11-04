a5b3545f5856392261f539c89e3f632dfa9c1ffb
cjackychen <jzc5521@psu.edu>
2016-11-04 14:30:56 -0400

    Update FanTwoOff.py

--- ClimateControlSystem/Python/FanTwoOff.py
+++ ClimateControlSystem/Python/FanTwoOff.py
@@ -1,3 +1,8 @@
+# Author: Niravh Patel, Jacky Chen, Mohammed Ammouni
+# Date: 11/4/2016
+# Course: IST 440W
+# Purpose: Turn off Fan Two
+
 #import required Python libraries
 import RPi.GPIO as GPIO
 import time
