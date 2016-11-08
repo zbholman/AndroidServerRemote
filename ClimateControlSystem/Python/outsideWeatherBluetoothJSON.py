55446e6c488c76976fb6fa0dcdda4dd4eabb3d9b
MoAmmouni <mohammedammouni@gmail.com>
2016-11-07 23:01:33 -0500

    Update outsideWeatherBluetoothJSON.py

--- ClimateControlSystem/Python/outsideWeatherBluetoothJSON.py
+++ ClimateControlSystem/Python/outsideWeatherBluetoothJSON.py
@@ -1,46 +1,50 @@
 import json
-import pyowm #Python Open Weather Map
+
+import pyowm  # Python Open Weather Map
 import bluetooth
 
-#Bluetooth hexidecimal address of Raspberry Pi device
-bd_addr = "50:46:5D:1D:00:4C"
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
 port = 1
+server_sock.bind(("", port))
+server_sock.listen(1)
 
-sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-sock.connect((bd_addr, port))
+client_sock, address = server_sock.accept()
+print "Accepted connection from", address
 
-try:
-# Register API Key with source
- API_key = '89cb9b3c8c3c1ee033be08ffcfd26076'
- owm = pyowm.OWM(API_key)
+while True:
+    data = client_sock.recv(1024)
+    try:
+        # Register API Key with source
+        API_key = '89cb9b3c8c3c1ee033be08ffcfd26076'
+        owm = pyowm.OWM(API_key)
 
-# Register a location: city,state AND print
- location = 'Philadelphia,PA'
- print('Location:', location)
+        # Register a location: city,state AND print
+        location = 'Philadelphia,PA'
+        print('Location:', location)
 
-# Find a location to observe outdoor weather
- obs = owm.weather_at_place(location)
- w = obs.get_weather()
+        # Find a location to observe outdoor weather
+        obs = owm.weather_at_place(location)
+        w = obs.get_weather()
 
-# Check outdoor Fahrenheit temperature and print
- outdoorFahrenheit = w.get_temperature('fahrenheit')
- print('Fahrenheit:', outdoorFahrenheit['temp'])
+        # Check outdoor Fahrenheit temperature and print
+        outdoorFahrenheit = w.get_temperature('fahrenheit')
+        print('Fahrenheit:', outdoorFahrenheit['temp'])
 
-# Check outdoor Humidity percentage and print
- outdoorHumidity = w.get_humidity()
- print('Humidity:', outdoorHumidity, '%')
+        # Check outdoor Humidity percentage and print
+        outdoorHumidity = w.get_humidity()
+        print('Humidity:', outdoorHumidity, '%')
 
-# Create a dictionary object for gathering all data from PYOWM to write out to a JSON file
- outdoorTempTOjson = {'Location': location, 'Fahrenheit temperature': outdoorFahrenheit, 'Humidity': outdoorHumidity}
- fileName = 'outdoorWeather.json' # name of the JSON output file
- outFile = open(fileName, 'w') # W stands for writing, writing out to the JSON file
- json.dump(outdoorTempTOjson, outFile) # Dumping all contents from Temp to Json
-sock.send(outdoorTempTOjson)
- print ("sending [%s]" % outdoorTempTOjson)
- outFile.close() # Close the outfile
-
-except IOError:
- print("Error")
- 
- sock.close()
+        # Create a dictionary object for gathering all data from PYOWM to write out to a JSON file
+        outdoorTempTOjson = {'Location': location, 'Fahrenheit temperature': outdoorFahrenheit, 'Humidity': outdoorHumidity}
+        fileName = 'outdoorWeather.json'  # name of the JSON output file
+        outFile = open(fileName, 'w')  # W stands for writing, writing out to the JSON file
+        json.dump(outdoorTempTOjson, outFile)  # Dumping all contents from Temp to Json
+        print("sending [%s]" % outdoorTempTOjson)  # Print the contents of the JSON file being sent
+        outFile.close()  # Close the outfile
+        #What is the socket name??????
+        #socket.sendfile(outFile)
+        client_sock.close()
+        server_sock.close()
+    except IOError:
+        print("Error")
