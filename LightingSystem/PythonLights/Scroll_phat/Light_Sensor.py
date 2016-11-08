5bed6a3c8e4ec3ca6e201730960b33713029e6b5
Andrew Rooney <arr5365@psu.edu>
2016-11-08 14:29:24 -0500

    Added by Andrew, Automatic Lights

---
+++ LightingSystem/PythonLights/Scroll_phat/Light_Sensor.py
@@ -0,0 +1,56 @@
+import time
+import grovepi
+import running_lights #delete these files. No longer needed. 
+import high_beams #delete these files. No longer needed.
+import scrollphat
+
+# Connect the Grove Light Sensor to analog port A0
+# SIG,NC,VCC,GND
+light_sensor = 0
+
+# Connect the LED to digital port D4
+# SIG,NC,VCC,GND
+led = 4
+
+# Turn on LED once sensor exceeds threshold resistance
+threshold = 10
+
+grovepi.pinMode(light_sensor,"INPUT")
+grovepi.pinMode(led,"OUTPUT")
+
+highBeam = False
+lowBeam = False
+
+while True:
+    try:
+        # Get sensor value
+        sensor_value = grovepi.analogRead(light_sensor)
+
+        # Calculate resistance of sensor in K
+        resistance = (float)(1023 - sensor_value) * 10 / sensor_value
+
+        if resistance > sensor_value:
+
+            scrollphat.set_brightness(500)
+            #go through columns then rows, turning all pixels on
+            for y in range(5):
+                 for x in range(11):
+                      scrollphat.set_pixel(x,y,True)
+                      scrollphat.update()
+ 
+        else:
+
+	    
+            scrollphat.set_brightness(20)
+            #go through columns then rows, turning all pixels on
+            for y in range(5):
+                 for x in range(11):
+                      scrollphat.set_pixel(x,y,True)
+                      scrollphat.update()
+	
+            print("running lights")
+        print("sensor_value = %d resistance =%.2f" %(sensor_value,  resistance))
+        time.sleep(.5)
+
+    except IOError:
+        print ("Error")
