b9ebfe0c01c7444485db06db62936c0f6e093ac3
jharrison357 <jqh5373@gmail.com>
2016-11-08 11:34:05 -0500

    john test arrow

---
+++ LightingSystem/PythonLights/Scroll_phat/test_arrow.py
@@ -0,0 +1,27 @@
+import scrollphat
+
+
+scrollphat.set_brightness(10)
+
+
+scrollphat.set_pixel(0,2)
+scrollphat.set_pixel(1,2)
+scrollphat.set_pixel(2,2)
+scrollphat.set_pixel(3,2)
+scrollphat.set_pixel(4,2)
+scrollphat.set_pixel(5,2)
+scrollphat.set_pixel(6,2)
+scrollphat.set_pixel(7,2)
+scrollphat.set_pixel(8,2)
+scrollphat.set_pixel(9,2)
+scrollphat.set_pixel(10,2)
+
+
+scrollphat.set_pixel(7,0)
+scrollphat.set_pixel(8,1)
+scrollphat.set_pixel(8,4)
+scrollphat.set_pixel(7,5)
+
+
+
+scrollphat.update()
