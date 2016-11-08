471ebea1bbb4ee3bbf4302a5d06e0b8274b901c8
jharrison357 <jqh5373@gmail.com>
2016-11-08 11:39:59 -0500

    test arrow 3 john

--- LightingSystem/PythonLights/Scroll_phat/test_arrow.py
+++ LightingSystem/PythonLights/Scroll_phat/test_arrow.py
@@ -1,26 +1,31 @@
 import scrollphat
 
+import math
+import system
+import time
+
+
 
 scrollphat.set_brightness(10)
 
 
-scrollphat.set_pixel(0,2)
-scrollphat.set_pixel(1,2)
-scrollphat.set_pixel(2,2)
-scrollphat.set_pixel(3,2)
-scrollphat.set_pixel(4,2)
-scrollphat.set_pixel(5,2)
-scrollphat.set_pixel(6,2)
-scrollphat.set_pixel(7,2)
-scrollphat.set_pixel(8,2)
-scrollphat.set_pixel(9,2)
-scrollphat.set_pixel(10,2)
+scrollphat.set_pixel(0,2,1)
+scrollphat.set_pixel(1,2,1)
+scrollphat.set_pixel(2,2,1)
+scrollphat.set_pixel(3,2,1)
+scrollphat.set_pixel(4,2,1)
+scrollphat.set_pixel(5,2,1)
+scrollphat.set_pixel(6,2,1)
+scrollphat.set_pixel(7,2,1)
+scrollphat.set_pixel(8,2,1)
+scrollphat.set_pixel(9,2,1)
+scrollphat.set_pixel(10,2,1)
 
 
-scrollphat.set_pixel(7,0)
-scrollphat.set_pixel(8,1)
-scrollphat.set_pixel(8,4)
-scrollphat.set_pixel(7,5)
+scrollphat.set_pixel(7,0,1)
+scrollphat.set_pixel(8,1,1)
+scrollphat.set_pixel(8,4,1)
+scrollphat.set_pixel(7,5,1)
 
 
 
