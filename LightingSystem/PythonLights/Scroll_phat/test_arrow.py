df94feb00c93b222e6bc40ae0a350563007caccb
jharrison357 <jqh5373@gmail.com>
2016-11-08 11:48:56 -0500

    test arrow john

--- LightingSystem/PythonLights/Scroll_phat/test_arrow.py
+++ LightingSystem/PythonLights/Scroll_phat/test_arrow.py
@@ -9,8 +9,6 @@ scrollphat.set_brightness(10)
 
 
 scrollphat.set_pixel(0,2,1)
-
-
 scrollphat.set_pixel(1,2,1)
 scrollphat.set_pixel(2,2,1)
 scrollphat.set_pixel(3,2,1)
@@ -23,10 +21,10 @@ scrollphat.set_pixel(9,2,1)
 scrollphat.set_pixel(10,2,1)
 
 
-scrollphat.set_pixel(7,0,1)
-scrollphat.set_pixel(8,1,1)
-scrollphat.set_pixel(8,4,1)
-scrollphat.set_pixel(7,5,1)
+scrollphat.set_pixel(8,0,1)
+scrollphat.set_pixel(9,1,1)
+scrollphat.set_pixel(9,4,1)
+scrollphat.set_pixel(8,5,1)
 
 
 
