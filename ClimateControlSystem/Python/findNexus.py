6d1d291c8af277c9f8673f738f28f88f82865e46
MoAmmouni <mohammedammouni@gmail.com>
2016-11-10 10:35:44 -0500

    Update findNexus.py

--- ClimateControlSystem/Python/findNexus.py
+++ ClimateControlSystem/Python/findNexus.py
@@ -6,6 +6,8 @@ target_address = None
 
 nearby_devices = bluetooth.discover_devices()
 
+print (nearby_devices)
+
 for bdaddr in nearby_devices:
     if target_name == bluetooth.lookup_name( bdaddr ):
         target_address = bdaddr
