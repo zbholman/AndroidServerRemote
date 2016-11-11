a3d2417a11b4cb03e2a6518613b35bee49555425
sakifabu <sakifabu@live.com>
2016-11-10 21:00:08 -0500

    updated sftp of json dump

--- ConnectivityAndNotificationSystem/GetTxtfiletest.sh
+++ ConnectivityAndNotificationSystem/GetTxtfiletest.sh
@@ -2,14 +2,18 @@ SFTP_SERVER="192.168.0.101"# the server ip
 SFTP_USER="SakifAbu"# the user name for the server
 SFTP_PWD="sakifabu"# the password required for the user
 
-mkdir /home/user/Desktop/carfiles/carID100b7
-if [ "$?" = "1" ]; then
-        echo "Couldn't make a directory" 1>&2
+mkdir /home/user/Desktop/carfiles/carID100b7# making a directory to do the dump
+if [ "$?" = "1" ]; then #checking work status
+        echo "Couldn't make a directory" 1>&2# letting the user know if it failed
         exit 1
 fi
 
 lftp sftp://$SFTP_USER:$SFTP_PWD@$SFTP_SERVER  -e 'mirror -r /home/sakifabu/PSUABFA16IST440/MonitoringandLogging/logdump ~/home/user/Desktop/carfiles/carID100b7;'#lftp is a command-line file transfer program that supports sftp
 #it mirrors anything in that particular folder to the defined directory.
+if [ "$?" = "1" ]; then #checking work status
+        echo "Couldn't copy the files" 1>&2# letting the user know if it failed
+        exit 1
+fi
 
 
 
