8dc0f11abf73823590518e1b448b9c37f568feb9
sakifabu <sakifabu@live.com>
2016-11-10 20:58:07 -0500

    updated the sftp of json dump

--- ConnectivityAndNotificationSystem/GetTxtfiletest.sh
+++ ConnectivityAndNotificationSystem/GetTxtfiletest.sh
@@ -1,10 +1,25 @@
-ls | grep abc.txt
+SFTP_SERVER="192.168.0.101"# the server ip
+SFTP_USER="SakifAbu"# the user name for the server
+SFTP_PWD="sakifabu"# the password required for the user
+
+mkdir /home/user/Desktop/carfiles/carID100b7
 if [ "$?" = "1" ]; then
-        echo "Couldn't do annything" 1>&2
+        echo "Couldn't make a directory" 1>&2
         exit 1
 fi
-if [ "$?" = "0" ]; then
-        echo "Ok found the file"
-        scp pi@192.168.1.200:/home/pi/media/[somefile.txt] pi@192.168.1.200:/media
-        exit 1
-fi       
+
+lftp sftp://$SFTP_USER:$SFTP_PWD@$SFTP_SERVER  -e 'mirror -r /home/sakifabu/PSUABFA16IST440/MonitoringandLogging/logdump ~/home/user/Desktop/carfiles/carID100b7;'#lftp is a command-line file transfer program that supports sftp
+#it mirrors anything in that particular folder to the defined directory.
+
+
+
+#ls | grep abc.txt
+#if [ "$?" = "1" ]; then
+     #   echo "Couldn't do annything" 1>&2
+    #    exit 1
+#fi
+#if [ "$?" = "0" ]; then
+   #     echo "Ok found the file"
+  #      scp pi@192.168.1.200:/home/pi/media/[somefile.txt] pi@192.168.1.200:/media
+ #       exit 1
+#fi       
