e0688fe3afd67a2312aaf2933e603d59c2a8854c
jharrison357 <jqh5373@gmail.com>
2016-11-12 01:42:59 -0500

    john python unit tests

---
+++ LightingSystem/PythonLights/Scroll_phat/test_paint.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class TestPaint(TestCase):
+    #return true
+    def test_paint(self):
+        self.assertTrue(25)
+    #return true
+    def test_paint(self):
+        self.assertEqual(100,100)
+    #return true
+    def test_paint(self):
+        self.assertNotEqual(100,200)
