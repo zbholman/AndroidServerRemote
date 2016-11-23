d622e6a50207d81bea416195fe74590f728cf924
Keval Patel <kpp5104@psu.edu>
2016-11-22 21:22:54 -0500

    Update CNS.py

--- ConnectivityAndNotificationSystem/CNS.py
+++ ConnectivityAndNotificationSystem/CNS.py
@@ -17,4 +17,20 @@ def msgQueue():
 		sendToNotification(item) #Forwards message to sTMAL function.
 q.task_done() #Task done, what a champ.
 
+#Send the message to monitoring and logging. Can use basic ports for this.
+def sendToNotification(message):
+	connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+	channel = connection.channel()
 
+	channel.queue_declare(queue='Notifications System', durable=True)
+
+	message = ' '.join(sys.argv[1:]) or "Notifications System"
+	channel.basic_publish(exchange='',
+                     	 routing_key='Notifications System',
+                     	 body=message,
+                     	 properties=pika.BasicProperties(
+                	        delivery_mode = 2, # make message persistent
+        	              ))
+	print(" [x] Sent %r" % message)
+connection.close()
