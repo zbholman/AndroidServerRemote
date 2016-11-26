5409cf7d48863d369fa83e949f7d12ad84b55531
Andrew Rooney <arr5365@psu.edu>
2016-11-26 14:33:38 -0500

    consolidated turn signals

---
+++ LightingSystem/LedBarLights/Left_Turn_On.py
@@ -0,0 +1,32 @@
+import time
+import grovepi
+
+# DI,DCKI,VCC,GND
+ledbar = 6
+
+grovepi.ledBar_init(ledbar, 0)
+grovepi.pinMode(ledbar,"OUTPUT")
+i = 0
+grovepi.ledBar_orientation(ledbar, 0)
+
+# ledbar_setLed(pin,led,state)
+# led: which led (1-10)
+# state: off or on (0,1)
+
+# ledbar_setLevel(pin,level)
+# level: (0-10)
+
+while True:
+    try:
+        for i in range(3, 10):
+            grovepi.ledBar_setLed(ledbar, i, 1)
+            time.sleep(.2)
+
+        for i in range(3, 10):
+            grovepi.ledBar_setLed(ledbar, i, 0)
+            time.sleep(.2)
+
+    except (KeyboardInterrupt, SystemExit):
+        for i in range(3, 10):
+            grovepi.ledBar_setLed(ledbar, i, 0)
+        break
