21223b30739a58c4af8ebadd60010ba8f07d8e88
Andrew Rooney <arr5365@psu.edu>
2016-11-26 20:11:09 -0500

    Fixed Queue

--- CommandAndControlSystem/queue.py
+++ CommandAndControlSystem/queue.py
@@ -1,16 +1,18 @@
 def worker():
-	while True:
-		item = q.get()
-		do_work(item)
-		q.task_done()
+        while True:
+                item = q.get()
+                do_work(item)
+                q.task_done()
 
-q = Queue()
-for i in range(num_worker_treads):
-	t = Thread(target=worker)
-	t.daemon = True
-	t.start()
+def Queue(self):
+        q = Queue(maxsize = 0)
+        for i in range(5):
+                t = Thread(target=worker)
+                t.daemon = True
+                t.start()
 
-for item in source():
-	q.put(item)
+        for item in source():
+                q.put(item)
 
-q.join()
+        q.join()
+
