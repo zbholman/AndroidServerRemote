89d5f4257fe24710c5614168b4d26397df820fc7
Ivan Iakimenko <isirc10@gmail.com>
2016-11-30 16:26:56 -0500

    Fixed bugs with watchdog, added more functionality

--- MonitoringAndLoggingSystem/Monitor.py
+++ MonitoringAndLoggingSystem/Monitor.py
@@ -19,10 +19,14 @@ subsys_ids = {'brs':'Braking System','clc':'Climate Control System', 'ems':'Ener
 class Monitor:
     
 	#stores last time subsystems sent out message to comm. bus
-    subsys_last_heard = {'brs':0,'clc':0, 'ems':0, 'ccs':0, 'cns':0, 'dts':0, 'lts':0, 'sac':0}
+    subsys_last_heard = {}
 
     #stores last known health code of subsystem
-	subsys_last_hc = {'brs':3,'clc':3, 'ems':3, 'ccs':3, 'cns':3, 'dts':3, 'lts':3, 'sac':3}
+	subsys_last_hc = {}
+	
+	#stores whether subsystem is in timeout status
+	#levels: no, yes, dead
+	subsys_timeout = {}
 	
     #time in milliseconds for watchdog to ask for subsystem status
     timeout_warning = 5000
@@ -36,30 +40,41 @@ class Monitor:
 
     #This method initializes Monitor instance, setting the "last_heard" 
     #times for each subsystem to the time when the monitor was started
+	#also initializes the starting subsystem health codes dictionary
     def __init__(self):
         self.start_monitor_time = datetime.datetime.now()
-        for subsys in subsys_ids:
+        for subsys in subsys_ids.keys():
             self.subsys_last_heard[subsys] = self.start_monitor_time
+			self.subsys_last_hc[subsys] = 3
+			self.subsys_timeout[subsys] = 'no'
+		
+		#add a loop here that continuously checks watchdog for unresponsive systems
 	
 	#This method should be called by the message threading queue in order
 	#to process the message, update watchdogs, interface with CNS and Logger
+	#must recieve a Message object, not just the json string
     def ProcessMessage(self, recieved_message):
 		Log.Parse_Message_To_Collection(recieved_message)
 		SetLastHeardFrom(recieved_message.Return_Origin_ID())
 		if(recieved_message.Return_Payload()[:2:] == "HC"):
+			#update last known HC of subsystem
 			#Do something to pass message to cns 
 
     #This method sends a ping to the destination subsystem as a status
 	#request to see that the subsystem is still functioning and healthy
 	#To implement, create a new message object and populate it with the
 	#correct data (universal code needed for status update request)
+	#also logs the fact that a system was pinged
     def PingSystem(self, destination_id):
         toSend = Message('car123', 'mls', 'destination_id', '100', 'Status Update')
+		Log.Parse_Message_To_Collection(toSend)
+		#add something to actually send the message out to the subsystem
     
     #This method sets the last time the subsystem in question broadcasted 
 	#a message. This is vital to reset this time for the watchdog to work
     def SetLastHeardFrom(self, origin_id):
         self.subsys_last_heard[origin_id] = datetime.datetime.now()
+		self.subsys_timeout[origin_id] = 'no'
         
     #This method returns the last time the subsystem in question broadcaste a message. 
     def GetLastHeardFrom(self, subsystem_id):
@@ -71,7 +86,12 @@ class Monitor:
 	#attempt to see if everything is still functioning properly. If even
 	#more time passes, this method calls ThrowUnresponsiveError()
     def CheckWatchdog(self):
-        pass
+		current_time = datetime.datetime.now()
+        for subsys in subsys_ids.keys():
+            if(current_time - self.subsys_last_heard[subsys] > timeout_warning and self.subsys_timeout[subsys] == 'no'):
+				PingSystem(subsys)
+				self.subsys_timeout[subsys] = 'yes'
+			else if(current_time - self.subsys_last_heard[subsys] > timeout_critical and self.subsys_timeout[]):
     
     #This method sends out an error message to warn the driver that a 
 	#subsystem is unresponsive if it hasn't sent a status update over 
