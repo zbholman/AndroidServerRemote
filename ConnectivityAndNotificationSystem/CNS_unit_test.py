de29780ac3e0f8b20ee6098a19c37eb764bab4c6
Sydnee Woodhouse <svwoodhouse@gmail.com>
2016-12-01 19:44:05 -0500

    unit test for CNS file

---
+++ ConnectivityAndNotificationSystem/CNS_unit_test.py
@@ -0,0 +1,14 @@
+import unnitest
+from CNS import msgQueue
+from CNS import sendToNotification
+
+class CNSTestCase(unittest.TestCase):
+
+  def test_msgQueue(self):
+    self.assertTrue(msgQueue())
+    
+  def test_sendToNotification(self):
+    self.assertTrue(sendToNotifiation('CNS'))
+    
+if __name__ == '__main__':
+  unittest.main()
