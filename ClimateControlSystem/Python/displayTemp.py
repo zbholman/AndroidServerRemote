8f04ffdb06cb2811269b5a36ba6f9685433426be
cjackychen <jzc5521@psu.edu>
2016-12-02 15:25:12 -0500

    Update DisplayTemp
    Made comments

--- ClimateControlSystem/Python/displayTemp.py
+++ ClimateControlSystem/Python/displayTemp.py
@@ -1,36 +1,36 @@
 import bluetooth
 import RPi.GPIO as GPIO
 import time
-import os
-import glob
-import time
+import os #Enable wire drivers and interface with sensor
+import glob #Load drivers
+import time #Define time
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(24,GPIO.OUT)
 
-os.system('modprobe w1-gpio') 
+os.system('modprobe w1-gpio')  
 os.system('modprobe w1-therm')
 
 #Gets the ID of temperature sensor
-base_dir = '/sys/bus/w1/devices/'
+base_dir = '/sys/bus/w1/devices/' #Temperature serial code
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
 #Opens folder for temperature sensor
-def read_temp_raw():
-    f = open(device_file, 'r')
+def read_temp_raw(): #Define variable for raw temperature
+    f = open(device_file, 'r') #Open, read, and record temperature file
     lines = f.readlines()
     f.close()
     return lines
 
 #Reads the temperature coming from sensor
 def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
+    lines = read_temp_raw() #Grabs data from the temperature folder
+    while lines[0].strip()[-3:] != 'YES': #Keeps on reading temperature for 'YES' signal
+        time.sleep(0.2) #If it fails for 'YES' signal then it restarts
         lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
+    equals_pos = lines[1].find('t=') #Find temperature output from sensor
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0 #Celsius temperature 
