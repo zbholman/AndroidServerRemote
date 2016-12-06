94cec93881b531fcd1afbc89a4abeb3476aa0b30
sakifabu <sakifabu@live.com>
2016-12-05 23:05:05 -0500

    updated Pulse

--- BrakingSystem/sensor_stop.py
+++ BrakingSystem/sensor_stop.py
@@ -13,7 +13,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(11,GPIO.OUT)
 
-p = GPIO.PWM(11,50)#PWM'Post-width Modulation' puts pin 11 to 50Hz
+p = GPIO.PWM(11,50)#PWM'Pulse-width Modulation' puts pin 11 to 50Hz
 p.start(9)
 try:
     while True:
