7e435bfda301781d5151c969419763dc8bb2d8fd
Phuong Lu <plu396111@gmail.com>
2016-12-07 23:20:00 -0500

    Edited subprocess filepath

--- SafetyAndAccessControlSystem/touchSensor.py
+++ SafetyAndAccessControlSystem/touchSensor.py
@@ -2,7 +2,6 @@ import time
 import RPi.GPIO as GPIO
 import subprocess
 
-
 GPIO.setmode(GPIO.BCM)
 
 padPin=18
@@ -12,5 +11,5 @@ while True:
     padPressed = GPIO.input(padPin)
 
     if padPressed:
-        subprocess.Popen("/usr/bin/python /home/pi/PSUABFA16IST440/SafetyAndAccessControlSystem/Doors_unlock.py", shell=True)
+        subprocess.Popen("/usr/bin/python /home/pi/PSUABFA16IST440/SafetyAndAccessControlSystem/SenseHat/Doors_unlock.py", shell=True)
     time.sleep(0.5)
