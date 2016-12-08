c2043d687fd00764c540294b11edbe1cff46973f
Jaein Lee <jol5460@psu.edu>
2016-12-08 02:39:17 +0000

    Create touchSensor.py

---
+++ SafetyAndAccessControlSystem/touchSensor.py
@@ -0,0 +1,16 @@
+import time
+import RPi.GPIO as GPIO
+import subprocess
+
+
+GPIO.setmode(GPIO.BCM)
+
+padPin=18
+GPIO.setup(padPin,GPIO.IN)
+
+while True:
+    padPressed = GPIO.input(padPin)
+
+    if padPressed:
+        subprocess.Popen("/usr/bin/python /home/pi/PSUABFA16IST440/SafetyAndAccessControlSystem/Doors_unlock.py", shell=True)
+    time.sleep(0.5)
