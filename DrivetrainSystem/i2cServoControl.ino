ad8a04a8c3f237ebbc1e69b4ded8a58e78d03b9f
Ivan Iakimenko <isirc10@gmail.com>
2016-12-14 11:41:24 -0500

    Added servo control files for arduino mini

---
+++ DrivetrainSystem/i2cServoControl.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+#include <Servo.h> 
+#define SLAVE_ADDRESS 0x07
+
+Servo steering;
+Servo gas;
+int selector = 0;
+boolean secondByte = false;
+
+void setup() {
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+  // define callbacks for i2c communication
+  Wire.onReceive(setServo);
+  Serial.begin(9600); // start serial for output
+  steering.attach(5);
+  gas.attach(6);
+}
+
+void loop() {
+}
+
+// callback for received data
+void setServo(int byteCount) {
+  while (Wire.available()) {
+
+    int number = (int)Wire.read();
+    if(number>180){
+      secondByte = true;
+      selector = number;
+    }
+    else if(secondByte){
+      if(selector == 200){ //Gas
+        Serial.print("set gas servo to: ");
+        Serial.println(number);
+        gas.write(number);
+        selector = 0;
+      }
+      else if(selector == 201){
+        Serial.print("set steering servo to: ");
+        Serial.println(number);
+        steering.write(number);
+        selector = 0;
+      }
+      
+      secondByte = false;
+    }
+    
+  }
+}
+
