55f5433f938368a42d3374857ae9a6f9034674d6
sakifabu <sakifabu@live.com>
2016-12-14 13:23:58 -0500

    upadate braking off

--- BrakingSystem/braking_off.py
+++ BrakingSystem/braking_off.py
@@ -13,7 +13,21 @@ GPIO.setup(11,GPIO.OUT)#
 
 p = GPIO.PWM(11,50)#PWM'Pulse-width Modulation' puts pin 11 to 50Hz
 p.start(5)
-while True :
-	p.ChangeDutyCycle(9)#disengage brakes after 2 seconds
+loopStart =  time.time()   #time when program starts
+timePassed = 0.0
+try:
+        while timePassed < 2:
+                now = time.time()   #current time
+                timePassed = now - loopStart   #current time minus time program started
+                timePassed = int(timePassed)   #changes timePassed into Integer
+		p.ChangeDutyCycle(9)#disengage brakes after 2 seconds
+		if timePassed >= 4 :
+			p.stop()   #stops power to servo after 4 seconds
+			GPIO.cleanup()
+except KeyboardInterrupt:
+        GPIO.cleanup()
+        p.stop()
+#while True :
+#	p.ChangeDutyCycle(9)#disengage brakes after 2 seconds
 #p.stop()#stops power to servo
 #GPIO.cleanup()
