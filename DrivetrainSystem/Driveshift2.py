f4232d66048f838fdf375242ab7de1d039efbf65
Ivan Iakimenko <isirc10@gmail.com>
2016-12-13 23:10:17 -0500

    Update Driveshift2.py

--- DrivetrainSystem/Driveshift2.py
+++ DrivetrainSystem/Driveshift2.py
@@ -11,15 +11,16 @@ from ArduinoServo import ServoControl
 import time
 import sys
 
+Servo = ServoControl()
 
 # Configure min and max servo pulse lengths
 servo_min = 0  # Min pulse length out of 4096 
 servo_max = 180  # Max pulse length out of 4096
 servo_neutral = 90 #The max pulse length of neutal
-servo_drive = 0 #The value to be changed for speed
+servo_drive = servo_neutral #The value to be changed for speed
 
 # To quit
-print('Moving servo on channel 0, press Ctrl-C to quit...')
+print('press Ctrl-C to quit...')
 print("w = forward")
 print("a = turn left")
 print("s = reverse")
@@ -36,54 +37,49 @@ while True:
 
     if (mode == 'p'): #Sets car into park, place holder code
 	    print('Stop')
-	    pwm.set_pwm(0, 0, servo_neutral)
-	    
-	    
+	    Servo.setGas(servo_neutral)
+		
     elif (mode == 's'): #Sets car into reverse
-            pwm.set_pwm(1, 1, steering_center)
-	    if servo_drive > 375:
-		    servo_drive = servo_drive - (((servo_drive - 375) * 2) + 16)
-	    pwm.set_pwm(0, 0, servo_drive)
-	    
+        Servo.setGas(servo_neutral)
+	    #if servo_drive > servo_neutral:
+		#    servo_drive = servo_drive - (((servo_drive - 375) * 2) + 16)
+		servo_drive = 92
+	    Servo.setGas(servo_drive)
 	    
     elif (mode == 'w'): #Sets car into drive
-            pwm.set_pwm(1, 1, steering_center)
-	    if servo_drive < 359:
-		    servo_drive = servo_drive + (((359 - servo_drive) * 2) + 16)
-	    pwm.set_pwm(0, 0, servo_drive)
-	    
+        Servo.setGas(servo_neutral)
+	    #if servo_drive < 359:
+		#    servo_drive = servo_drive + (((359 - servo_drive) * 2) + 16)
+		servo_drive = 92
+	    Servo.setGas(servo_drive)
 	    
     elif (mode == 'n'): #Sets car into neutral 
-	    pwm.set_pwm(0, 0, 375)
-	    
+	    Servo.setGas(servo_neutral)
 	    
     elif (mode == 'f'): #Speeds up the car
-	    if servo_drive <= 375 and servo_drive > 359:
-		    servo_drive = 375
-	    servo_drive = servo_drive + 10
-	    pwm.set_pwm(0, 0, servo_drive)
+	    if servo_drive <= servo_neutral or servo_drive >= servo_max:
+		    servo_drive = servo_neutral
+	    servo_drive = servo_drive + 1
+	    Servo.setGas(servo_drive)
 	    
-
-    elif (mode == 'g'): #Speeds down the car
-	    if servo_drive < 375 and servo_drive >= 359:
-		    servo_drive = 359
-	    servo_drive = servo_drive - 10
-	    pwm.set_pwm(0, 0, servo_drive)
+    elif (mode == 'g'): #Slows down the car
+	    if servo_drive <= servo_neutral:
+		    servo_drive = servo_neutral
+	    servo_drive = servo_drive - 1
+	    Servo.setGas(servo_drive)
 
     elif (mode == 'a'): #Turns the car left
-            pwm.set_pwm(1, 1, steering_left)
+        Servo.setSteering(servo_min)
 
     elif (mode == 'c'): #Sets the steering to center
-            pwm.set_pwm(1, 1, steering_center)
-
+        Servo.setSteering(servo_neutral)
+			
     elif (mode == 'd'): #Turns the car right
-            pwm.set_pwm(1, 1, steering_right)
+        Servo.setSteering(servo_max)
 	    
-
-
     elif (mode == 'q'): #Exits gear shift
-	    pwm.set_pwm(0, 0, servo_neutral)
-	    pwm.set_pwm(1, 1, steering_center)
+	    Servo.setGas(servo_neutral)
+	    Servo.setSteering(servo_neutral)
 	    print('Good Bye')
 	    sys.exit()
 	    
